d9a3847585d92d0a1173c85884a6de883dfdba60
Keith Scheldt <keithscheldt@gmail.com>
2023-07-16 16:26:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,151 @@
+import { Configuration, OpenAIApi } from 'openai'
+import { process } from './env'
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, get, remove } from "firebase/database";
+
+// set openai configuration
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+// init openai
+const openai = new OpenAIApi(configuration)
+
+// set Firebase configuration
+const appSettings = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase app
+const app = initializeApp(appSettings)
+
+
+// Get a reference to the database service
+const database = getDatabase(app)
+
+// reference to converstaion in db
+const conversationInDb = ref(database)
+
+// select chatbotConverstation div in the form
+const chatbotConversation = document.getElementById('chatbot-conversation')
+
+// Chatbot instruction - system message
+const instructionObj = {
+    role: 'system',
+    content: 'You are wise old owl that answers questions and gives advise. You try to rhyme when it makes sense.'
+};
+
+
+
+// submit event listener
+document.addEventListener('submit', (e) => {
+    e.preventDefault() // prevent page refresh
+    const userInput = document.getElementById('user-input') // select user input.
+
+
+    // add user input to conversation array
+    // push using firebase push method
+    push(conversationInDb, {
+        role: 'user',
+        content: userInput.value
+    })
+    // fetch reply
+    fetchReply()
+    // create new speech bubble for each message
+    const newSpeechBubble = document.createElement('div') // create new speech bubble (div)
+    newSpeechBubble.classList.add('speech', 'speech-human') // add speech and speech-human classes to new speech bubble
+    chatbotConversation.appendChild(newSpeechBubble) // add new speech bubble to chatbotConversation
+    newSpeechBubble.textContent = userInput.value // add text to the speech bubble
+    userInput.value = '' // clear user input
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight // scroll to bottom of chat
+})
+
+// fetchReply
+fetchReply()
+function fetchReply() {
+    // callback - snapshot of data as it exists in the database
+    get(conversationInDb).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            // console.log(snapshot)
+            // console.log(snapshot.val())
+            // console.log(Object.values(snapshot.val()))
+            const conversationArray = Object.values(snapshot.val())
+            // add instructionObj to conversationArray
+            conversationArray.unshift(instructionObj)
+
+            const response = await openai.createChatCompletion({
+                model: 'gpt-4',
+                messages: conversationArray,
+                temperature: 1,
+                presence_penalty: 0,
+                frequency_penalty: 0.3,
+            })
+            console.log(response.data.choices[ 0 ].message)
+            //  we want to add the whole response.data.choices[0] object so leave off the .content
+            // Add the completion to the database
+            push(conversationInDb, response.data.choices[ 0 ].message)
+
+
+            // pass completion to typewriter function
+            renderTypewriterText(response.data.choices[ 0 ].message.content)
+
+        } else {
+            console.log('No data available');
+        }
+    })
+
+
+};
+
+
+// typewriter effect
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i - 1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
+
+// clear conversation
+document.getElementById('clear-btn').addEventListener('click', () => {
+    remove(conversationInDb)
+    chatbotConversation.innerHTML = '<div class="speech speech-ai">How can I help you?</div>'
+})
+
+// renderConversaionFromDB - render conversation from database
+function renderConversaionFromDB() {
+    // get conversation from database
+    get(conversationInDb).then(async(snapshot) => {
+        if(snapshot.exists()) {
+            Object.values(snapshot.val()).forEach((message) => {
+                const newSpeechBubble = document.createElement('div')
+                newSpeechBubble.classList.add(
+                    'speech', 
+                    `speech-${dbOBj.role === 'user' ? 'human' : 'ai'} }`
+                    )
+                    chatbotConversation.appendChild(newSpeechBubble)
+                    // add text to speech bubble
+                    newSpeechBubble.textContent = dbObj.content
+            })
+            // scroll to bottom of chat
+            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+        }
+    })
+}
+renderConversaionFromDB()
---
+++ output.md
@@ -0,0 +1,43 @@
+<!-- Output file from frequency tests -->
+
+1. Unfortunately, your finances don't currently allow for that purchase.
+2. It seems like that might be out of your current budget.
+3. That purchase doesn't align with your present financial situation.
+4. Your bank account might not support this expenditure.
+5. Given your current financial status, that may not be the best investment. 
+6. That item might be beyond your financial reach at the moment.
+7. It's important to stick to your budget; this purchase may exceed those limits.
+8. To maintain your financial health, you may need to refrain from this purchase.
+9. It's best to hold on making non-essential purchases due to your current financial conditions.
+10. Your current budget might not be able to stretch for that expense.
+11. It's advisable to manage your resources more prudently; this doesn't seem like a viable purchase right now.
+12. Given your existing monetary landscape, it's not the best time to acquire that.
+13. Please reconsider your budget limitations before opting for this purchase.
+14. This item seems to exceed your current financial parameters.
+15. It's crucial to stick to your fiscal plan; you might want to revaluate this expenditure.
+16. Right now, your financial resources may fall short for this purchase.
+17. Your current financial standing might not comfortably accommodate that expense.
+18. Presently, that purchase doesn't seem to fit into your budget.
+19. It's important to keep your spending within budget limits, which might not allow for this item.
+20. Acquiring that may not be the best financial decision given your current scenario.
+
+1. You might want to re-evaluate your purchase, considering the current state of your finances.
+2. I hate to be the bearer of bad news, but you lack sufficient funds for this item at present.
+3. Unfortunate as it may sound, your budget doesn't allow for such a transaction right now.
+4. Prudent budgeting means setting priorities and in this case, you may have to pass on this one.
+5. It is wise not to stretch ourselves financially on non-essential items if we can avoid it.
+6. Financial responsibility sometimes requires us to refrain from unnecessary purchases.
+7. Unfortunately, the price tag on that exceeds your monetary capabilities.
+8. Given your current economic condition, this purchase seems unlikely these days.
+9. Your financial situation appears quite tight; it's advisable to rethink this purchasing decision at this time.
+10. Checking out with empty pockets can lead to many complications; let's reconsider the benefits over cost.
+11. Tighten the purse strings a bit - your wallet might appreciate it more.
+12. A thorough review of your account balance makes me advise against this acquisition right now.
+13. Unfortunately, this item falls outside of our affordable range presently.
+14. Living within one's means promotes wiser spending habits. This purchase should probably wait for better times.
+15. No need to rock the fiscal boat. Let's leave this luxury item behind for another day.
+16. Due to the lean contents of your pocket book, making this buy would prove impractical.
+17. Money-saving moves often preclude purchases like this; let's save instead.
+18. We must remember that caution prevents penniless predicaments. 
+19. One best avoids deepening a hole when already down; this might not be a viable purchase today.
+20. You seem to be cash-strapped; perhaps prioritizing needs over wants would be recommended at this point.
